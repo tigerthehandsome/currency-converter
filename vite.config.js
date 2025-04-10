3243d8ab10304f162108fab32e1de6eb83b382ff
tigerthehandsome <145098737+tigerthehandsome@users.noreply.github.com>
2025-04-10 12:38:12 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  plugins: [react()],
   root: '.',
+  publicDir: 'public',
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: 'public/index.html'
+    }
   },
   server: {
-    port: 3000,
-  },
+    port: 3000
+  }
 })
